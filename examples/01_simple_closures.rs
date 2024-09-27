cd727fabee2ac4f013453143f0de43d4fb0c391f
Mathias <email@example.com>
2024-09-27 07:09:06 +0000

    save

---
+++ examples/01_simple_closures.rs
@@ -0,0 +1,26 @@
+
+use chrono::Utc;
+use chrono::TimeZone;
+
+
+const DATA: [f64; 14] = [
+    137.24, 136.37, 138.43, 137.41, 139.69, 140.41, 141.58, 139.55, 139.68, 139.1, 138.24, 135.67,
+    137.12, 138.12,
+];
+
+fn main() {
+    println!("simple closures");
+
+    let one_closures = (0..).zip(DATA.iter()).map(|(idx, price)| {
+        let day = (idx / 5) * 7 + (idx % 5) + 1;
+        #[allow(deprecated)]
+        let date = Utc.ymd(2019, 10, day);
+
+        println!("DEBUG 2: {},{}", date, price);
+        
+        (date, *price)
+    });
+
+    println!("{:?}",one_closures);
+}
+
